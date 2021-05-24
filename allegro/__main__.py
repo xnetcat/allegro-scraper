ae2443fa0f555c0f234cd6738b3bdf0734db5abc
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-05-24 12:36:57 +0200

    placeholder for console_entry_point

---
+++ allegro/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog="spotdl-spider",
+        description="Allegro spider, product scrapper",
+    )
+    parser.add_argument("queries", type=str, nargs="+", help="search queries")
+
+    return parser.parse_args()
+
+def console_entry_point():
+    arguments = parse_arguments()
+
+    for query in arguments.queries:
+        print(query)
+
+if __name__ == '__main__':
+    console_entry_point()
