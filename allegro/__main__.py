3e0ba9420c928b9f519761bdd35277a82229d58f
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-06-06 14:06:46 +0200

    added logger

--- allegro/__main__.py
+++ allegro/__main__.py
@@ -1,9 +1,11 @@
 import argparse
 import json
+import logging
 
 from dataclasses import asdict
 from allegro.search.product import Product
-from allegro.search.crawl import crawl
+from allegro.search import crawler
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -11,31 +13,54 @@ def parse_arguments():
         description="Allegro spider, product scrapper",
     )
     parser.add_argument("queries", type=str, nargs="+", help="search queries")
-    parser.add_argument("--output", "-o", help=r"Output file ex. C:/test/file.json", required=True)
+    parser.add_argument(
+        "--output", "-o", help=r"Output file ex. C:/test/file.json", required=True
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="store_true",
+        default=False,
+        help="Will print more logging messages",
+    )
 
     return parser.parse_args()
 
 
 def console_entry_point():
     arguments = parse_arguments()
-    products = []
 
+    # Set up logging
+    logging.basicConfig(
+        level=logging.DEBUG if arguments.verbose else logging.INFO,
+        format="%(asctime)s :: %(module)s :: [%(levelname)s] %(message)s"
+        if arguments.verbose
+        else "[%(levelname)s] %(message)s",
+    )
+
+    # List containing Product objects
+    products = []
     for query in arguments.queries:
+        # Single allegro offer
         if "allegro.pl/oferta/" in query:
             product = Product.from_url(query)
             products.append(product)
         else:
-            results = crawl(query)
+            # Search term (we get only first page of results)
+            results = crawler.search(query)
             products.extend(results)
 
-    new_list = []
-    for product in products:
-        new_list.append(asdict(product))
+    # Convert products to dicts
+    products = [asdict(var) for var in products if var is not None]
 
-    json_dump = json.dumps(new_list, indent=4, ensure_ascii=False)
+    # Dump dicts to json string
+    json_dump = json.dumps(products, indent=4, ensure_ascii=False)
 
+    # Save json to file
     with open(arguments.output, "w", encoding="utf-8") as output:
+        logging.info(f"Saving to {arguments.output}")
         output.write(json_dump)
+
 
 if __name__ == "__main__":
     console_entry_point()
