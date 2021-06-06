9943af7f115178da6c9ff9869472db84373dd65f
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-06-06 22:03:58 +0200

    starting crawler development

--- allegro/search/crawler.py
+++ allegro/search/crawler.py
@@ -3,10 +3,11 @@ import logging
 
 from bs4 import BeautifulSoup
 from allegro.search.product import Product
+from allegro.types.crawler import Options
 from typing import List
 
 
-def search(search_term: str, proxies=None) -> List[Product]:
+def search(search_term: str, proxies: dict = None) -> List[Product]:
     """
     ### Args
     - search_term: `str` name of the searched item
@@ -14,7 +15,11 @@ def search(search_term: str, proxies=None) -> List[Product]:
     ### Returns
     - `List[Product]` list containing scrapped products
     """
+
+    # Products list
     products = []
+
+    # Default headers
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",  # noqa: E501
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -26,12 +31,15 @@ def search(search_term: str, proxies=None) -> List[Product]:
         "Cache-Control": "no-cache",
     }
 
-    req = requests.get(
+    # Send http GET request
+    request = requests.get(
         f"https://allegro.pl/listing?string={search_term}",
         headers=headers,
         proxies=proxies,
     )
-    soup = BeautifulSoup(req.text, "html.parser")
+
+    # Parse html with BeautifulSoup
+    soup = BeautifulSoup(request.text, "html.parser")
 
     sections = soup.find_all(
         "article",
@@ -54,13 +62,54 @@ def search(search_term: str, proxies=None) -> List[Product]:
                 f'Scraping "{product.name}" with url "{product.url}" [{index + 1}/{len(sections)}]'
             )
         else:
-            logging.info(
-                f'Scraping "{product.name}"'
-                f' [{index + 1}/{len(sections)}]'
-            )
+            logging.info(f'Scraping "{product.name}"' f" [{index + 1}/{len(sections)}]")
 
         # Add product to list
         products.append(product)
 
     # Return list with products
     return products
+
+
+def crawl(
+    search_term: str,
+    options: Options = None,
+    proxies: dict = None,
+) -> List[Product]:
+    """
+    ### Args
+    - search_term: `str` name of the searched item
+    - options: `Options` search options
+
+    ### Returns
+    - `List[Product]` list containing scrapped products
+    """
+
+    # No options so we default to results from first page
+    if options is None:
+        return search(search_term)
+
+    # Products list
+    products = []
+
+    # Default headers
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",  # noqa: E501
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+    }
+
+    # Send http GET request
+    request = requests.get(
+        f"https://allegro.pl/listing?string={search_term}",
+        headers=headers,
+        proxies=proxies,
+    )
+
+    # Parse html with BeautifulSoup
+    soup = BeautifulSoup(request.text, "html.parser")
