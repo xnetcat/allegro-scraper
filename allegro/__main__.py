64d86832f723bcb977035f5f71a10d1c8bb2de94
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-06-07 23:57:32 +0200

    moved argparser to separate module

--- allegro/__main__.py
+++ allegro/__main__.py
@@ -1,180 +1,11 @@
 from allegro.types.types_crawler import Filters
-import argparse
 import json
 import logging
 
 from dataclasses import asdict
 from allegro.search.product import Product
 from allegro.search import crawler
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        prog="allegro-spider",
-        description="Allegro spider, product scrapper",
-    )
-
-    # Search mode
-    parser.add_argument(
-        "--search",
-        "-s",
-        type=str,
-        nargs="+",
-        help="search queries"
-    )
-
-    # Crawl mode
-    parser.add_argument(
-        "--crawl",
-        "-c",
-        type=str,
-        nargs="+",
-        help="Enables crawling"
-    )
-
-    opts, _ = parser.parse_known_args()
-    if opts.search is None and opts.crawl is None:
-        parser.error("--crawl/-c or --search/-s is required")
-
-    # Sorting
-    parser.add_argument(
-        "--sorting",
-        "-so",
-        help="Sorting method",
-        choices={
-            "relevance_highest",
-            "price_from_lowest",
-            "price_from_highest",
-            "price_with_delivery_from_lowest",
-            "price_with_delivery_from_highest",
-            "popularity_highest",
-            "tome_to_end_least",
-            "time_added_latest"
-        }
-    )
-
-    # Allegro Smart! free shipping
-    parser.add_argument(
-        "--smart-free-shipping",
-        "-sfs",
-        action="store_true",
-        help="Allegro Smart! free shipping"
-    )
-
-    # Product condition
-    parser.add_argument(
-        "--product-condition",
-        "-pc",
-        nargs="+",
-        type=str,
-        help="Product condition",
-        choices={
-            "new",
-            "used",
-            "incomplete_set",
-            "new_without_tags",
-            "new_with_defect",
-            "after_return",
-            "aftermarket",
-            "regenerated",
-            "damaged",
-            "refurbished",
-            "for_renovation",
-            "not_requiring_renovation"
-        }
-    )
-
-    # Offer type
-    parser.add_argument(
-        "--offer-type",
-        "-ot",
-        nargs="+",
-        type=str,
-        help="Offer type",
-        choices={
-            "buy_now",
-            "auction",
-            "advertisement"
-        }
-    )
-
-    # Minimal price
-    parser.add_argument(
-        "--price-min",
-        "-pmin",
-        type=float,
-        help="Minimal price"
-    )
-
-    # Maximum price
-    parser.add_argument(
-        "--price-max",
-        "-pmax",
-        type=float,
-        help="Maximum price"
-    )
-
-    # Delivery time
-    parser.add_argument(
-        "--delivery-time",
-        "-dt",
-        help="Delivery time",
-        choices={
-            "today",
-            "one_day",
-            "two_day"
-        }
-    )
-
-    # Delivery methods
-    parser.add_argument(
-        "--delivery-methods",
-        "-dm",
-        nargs="+",
-        type=str,
-        help="Delivery methods",
-        choices={
-            "courier",
-            "inpost_parcel_locker",
-            "overseas_delivery",
-            "pickup_at_the_point",
-            "letter",
-            "package",
-            "pickup",
-            "email",
-        }
-    )
-
-    # Delivery options
-    parser.add_argument(
-        "--delivery-options",
-        "-do",
-        nargs="+",
-        type=str,
-        help="Delivery options",
-        choices={
-            "free_shipping",
-            "free_return"
-        }
-    )
-
-    # Output file
-    parser.add_argument(
-        "--output",
-        "-o",
-        help=r"Output file ex. C:/test/file.json",
-        required=True
-    )
-
-    # Verbose mode
-    parser.add_argument(
-        "--verbose",
-        "-v",
-        action="store_true",
-        help="Will print more logging messages",
-    )
-
-    return parser.parse_args()
+from allegro.argparser import parse_arguments
 
 
 def console_entry_point():
@@ -192,7 +23,7 @@ def console_entry_point():
     products = []
 
     # Search for specified products
-    if len(arguments.search) >= 1:
+    if arguments.search is not None and len(arguments.search) >= 1:
         for query in arguments.search:
             # Single allegro offer
             if "allegro.pl/oferta/" in query:
@@ -204,7 +35,7 @@ def console_entry_point():
                 products.extend(results)
 
     # Crawl specified search terms
-    if len(arguments.crawl) >= 1:
+    if arguments.crawl is not None and len(arguments.crawl) >= 1:
         for query in arguments.crawl:
             filters: Filters = {  # type: ignore
                 "sorting": arguments.sorting,
@@ -219,6 +50,7 @@ def console_entry_point():
             }
 
             results = crawler.crawl(query, filters=filters)
+            products.extend(results)
 
     # Convert products to dicts
     products = [asdict(var) for var in products if var is not None]
@@ -228,7 +60,7 @@ def console_entry_point():
 
     # Save json to file
     with open(arguments.output, "w", encoding="utf-8") as output:
-        logging.info(f"Saving to {arguments.output}")
+        logging.info(f"Saving {len(products)} products to {arguments.output}")
         output.write(json_dump)
 
 
---
+++ allegro/argparser.py
@@ -0,0 +1,193 @@
+from argparse import ArgumentParser
+
+
+def _parse_basic(parser: ArgumentParser):
+    # Search mode
+    parser.add_argument(
+        "--search",
+        "-s",
+        type=str,
+        nargs="+",
+        help="search queries"
+    )
+
+    # Crawl mode
+    parser.add_argument(
+        "--crawl",
+        "-c",
+        type=str,
+        nargs="+",
+        help="Enables crawling"
+    )
+
+    opts, _ = parser.parse_known_args()
+    if opts.search is None and opts.crawl is None:
+        parser.error("--crawl/-c or --search/-s is required")
+
+    return parser
+
+
+def _parse_misc(parser: ArgumentParser):
+    # Output file
+    parser.add_argument(
+        "--output",
+        "-o",
+        help=r"Output file ex. C:/test/file.json",
+        required=True
+    )
+
+    # Verbose mode
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="store_true",
+        help="Will print more logging messages",
+    )
+
+    return parser
+
+
+def _parse_filters(parser: ArgumentParser):
+    # Sorting
+    parser.add_argument(
+        "--sorting",
+        "-so",
+        help="Sorting method",
+        choices={
+            "relevance_highest",
+            "price_from_lowest",
+            "price_from_highest",
+            "price_with_delivery_from_lowest",
+            "price_with_delivery_from_highest",
+            "popularity_highest",
+            "tome_to_end_least",
+            "time_added_latest"
+        }
+    )
+
+    # Allegro Smart! free shipping
+    parser.add_argument(
+        "--smart-free-shipping",
+        "-sfs",
+        action="store_true",
+        help="Allegro Smart! free shipping"
+    )
+
+    # Product condition
+    parser.add_argument(
+        "--product-condition",
+        "-pc",
+        nargs="+",
+        type=str,
+        help="Product condition",
+        choices={
+            "new",
+            "used",
+            "incomplete_set",
+            "new_without_tags",
+            "new_with_defect",
+            "after_return",
+            "aftermarket",
+            "regenerated",
+            "damaged",
+            "refurbished",
+            "for_renovation",
+            "not_requiring_renovation"
+        }
+    )
+
+    # Offer type
+    parser.add_argument(
+        "--offer-type",
+        "-ot",
+        nargs="+",
+        type=str,
+        help="Offer type",
+        choices={
+            "buy_now",
+            "auction",
+            "advertisement"
+        }
+    )
+
+    # Minimal price
+    parser.add_argument(
+        "--price-min",
+        "-pmin",
+        type=float,
+        help="Minimal price"
+    )
+
+    # Maximum price
+    parser.add_argument(
+        "--price-max",
+        "-pmax",
+        type=float,
+        help="Maximum price"
+    )
+
+    # Delivery time
+    parser.add_argument(
+        "--delivery-time",
+        "-dt",
+        help="Delivery time",
+        choices={
+            "today",
+            "one_day",
+            "two_day"
+        }
+    )
+
+    # Delivery methods
+    parser.add_argument(
+        "--delivery-methods",
+        "-dm",
+        nargs="+",
+        type=str,
+        help="Delivery methods",
+        choices={
+            "courier",
+            "inpost_parcel_locker",
+            "overseas_delivery",
+            "pickup_at_the_point",
+            "letter",
+            "package",
+            "pickup",
+            "email",
+        }
+    )
+
+    # Delivery options
+    parser.add_argument(
+        "--delivery-options",
+        "-do",
+        nargs="+",
+        type=str,
+        help="Delivery options",
+        choices={
+            "free_shipping",
+            "free_return"
+        }
+    )
+
+    return parser
+
+
+def parse_arguments():
+    # Initialize argument parser
+    parser = ArgumentParser(
+        prog="allegro-spider",
+        description="Allegro spider, product scrapper",
+    )
+
+    # Parse basic arguments
+    parser = _parse_basic(parser)
+
+    # Parse misc arguments
+    parser = _parse_misc(parser)
+
+    # Parse filter arguments
+    parser = _parse_filters(parser)
+
+    # Return parsed arguments
+    return parser.parse_args()
