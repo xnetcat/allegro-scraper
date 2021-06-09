f19d86776d20ddad2afa54144d73f2f2f0ca911e
Jakub Kot <42355410+xnetcat@users.noreply.github.com>
2021-06-09 16:55:49 +0200

    added more types

--- allegro/types/options.py
+++ allegro/types/options.py
@@ -7,3 +7,5 @@ class Options(TypedDict):
     pages_to_fetch: Optional[int]
     start_page: Optional[int]
     avoid_duplicates: Optional[bool]
+    use_free_proxies: Optional[bool]
+    check_proxies: Optional[bool]
